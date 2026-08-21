5316752441f175779a0ad44a63d5920a5e9b675f
lucasfreitasbaptista-pixel <lucasfreitasbaptista@gmail.com>
2026-08-21 17:30:47 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.passosfisio.app"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
